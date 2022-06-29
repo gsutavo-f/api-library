0f39b7a8d8fc255134731aa6f69d6ce3366f1ece
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-06-29 00:44:11 -0300

    post method added to controller

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,17 +1,33 @@
-import books from '../models/Book.js';
+import Book from '../models/Book.js';
 
 class BookController {
 
    static getBooks(req, res) {
-      books.find((err, books) => {
+      Book.find((err, books) => {
          res.status(200).json(books);
       });
    }
 
-   static getBookById(id, req, res) {
-      books.findById(id, (err, books) => {
+   static getBookById(req, res) {
+      const {id} = req.body;
+      Book.findById(id, (err, books) => {
          res.status(302).json(books);
       })
+   }
+
+   static insertBook(req, res) {
+      let book = new Book(req.body);
+      book.save((err) => {
+         if (err) {
+            res.status(500).send(
+               {
+                  message: `${err.message} - fail to register book`
+               }
+            );
+         } else {
+            res.status(201).send(book.toJSON());
+         }
+      });
    }
 
 }
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,6 +4,8 @@ import BookController from "../controllers/bookController.js";
 const router = express.Router();
 
 router
-   .get('/books', BookController.getBooks);
+   .get('/books', BookController.getBooks)
+   .get('/books/:id', BookController.getBookById)
+   .post('/books', BookController.insertBook);
 
 export default router;
